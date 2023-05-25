f735ded507b7acf678b0deba8a3ec173696a716f
Wickhye <jihyesim170@gmail.com>
2023-05-25 10:33:34 +0900

    SET :: firebase 초기세팅

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FB_API_KEY,
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FB_API_ID,
+  measurementId: process.env.REACT_APP_FB_MAESUREMENT_ID,
+};
+
+// Initialize Firebase
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+export { firebase, firebaseApp, db };
