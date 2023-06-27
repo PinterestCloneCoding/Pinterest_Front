46658d179187f6eac120cf33127d8bd4dce4e818
Wickhye <jihyesim170@gmail.com>
2023-06-27 14:00:25 +0900

    STYLE :: Masonry 레이아웃

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,15 +1,27 @@
+import React from "react";
+import styled from "styled-components";
 import MediaPin from "../../components/common/MediaPin/MediaPin";
 import PinData from "../../mocks/dummy";
 
+import * as S from "./Main.style";
 
 const Main = () => {
-    return (
-        PinData && PinData.map((item) => (
+  return (
+    <S.Wrapper>
+      <S.Container>
+        {PinData &&
+          PinData.map((item) => (
             <MediaPin
-                title={item.title} pinImg={item.pinImg} profileImg={item.profileImg} userName={item.userName} 
+              key={item.id}
+              title={item.title}
+              pinImg={item.pinImg}
+              profileImg={item.profileImg}
+              userName={item.userName}
             />
-        ))
-    )
-}
+          ))}
+      </S.Container>
+    </S.Wrapper>
+  );
+};
 
 export default Main;
---
+++ src/pages/Main/Main.style.js
@@ -0,0 +1,21 @@
+import { styled } from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  margin-top: 15px;
+`;
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr); /* Four columns */
+  grid-template-rows: repeat(5, 1fr); /* Five rows */
+  grid-gap: 15px;
+  background-color: white;
+`;
