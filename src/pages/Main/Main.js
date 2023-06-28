bc08ff7df602ba67c9ac22f2f5aafe4420954029
Wickhye <jihyesim170@gmail.com>
2023-06-29 01:28:35 +0900

    FEAT :: 받아오는 거 추가, 근데 이거.. 로그인도 가져와야

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MediaPin from "../../components/common/MediaPin/MediaPin";
-import PinData from "../../mocks/dummy";
-
 import * as S from "./Main.style";
 import { Link } from "react-router-dom";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
+import { styled } from "styled-components";
 
 const Main = () => {
+  const [pinData, setPinData] = useState([]);
+
+  useEffect(() => {
+    const fetchPinData = async () => {
+      const pinsCollection = collection(db, "photo");
+      const pinsSnapshot = await getDocs(pinsCollection);
+      const newPinData = [];
+      pinsSnapshot.forEach((doc) => {
+        const pin = {
+          id: doc.id,
+          ...doc.data(),
+        };
+        newPinData.push(pin);
+      });
+      setPinData(newPinData);
+    };
+
+    fetchPinData();
+  }, []);
+
   const breakpointColumnsObj = {
     default: 5,
   };
@@ -13,22 +34,27 @@ const Main = () => {
   return (
     <S.Wrapper>
       <S.MyMasonryGrid breakpointCols={breakpointColumnsObj}>
-        {PinData &&
-          PinData.map((item) => (
-            <div key={item.id}>
-              <S.StyledLink to={`/pin/${item.id}`}>
+        {pinData &&
+          pinData.map((item, index) => (
+            <div key={index}>
+              <StyledLink to={`/pin/${item.id}`}>
                 <MediaPin
                   title={item.title}
-                  pinImg={item.pinImg}
+                  pinImg={item.imgUrl}
                   profileImg={item.profileImg}
                   userName={item.userName}
                 />
-              </S.StyledLink>
+              </StyledLink>
             </div>
           ))}
       </S.MyMasonryGrid>
     </S.Wrapper>
   );
 };
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #000;
+`;
 
 export default Main;
